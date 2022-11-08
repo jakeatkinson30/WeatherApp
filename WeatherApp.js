caf3401432e7d2d0c27f42698c6e2cb0f55b7ae7
jakeatkinson30 <jakea07@hotmail.com>
2022-11-08 10:20:52 +0000

    Updated filepath and renamed weatherapp.js

--- WeatherApp.js
+++ WeatherApp.js
@@ -4,7 +4,7 @@ const api = {
 }
 
 
-const searchbox = document.querySelector(".search-box");
+const searchbox = document.querySelector(".searchBox");
 searchbox.addEventListener("keypress", setQuery);
 
 function setQuery(e) {
@@ -22,20 +22,20 @@ function getResults (query) {
 }
 
 function displayResults(weather) {
-    let city = document.querySelector(".location .city");
+    let city = document.querySelector(".place .city");
     city.innerText = `${weather.name}, ${weather.sys.country}`;
 
     let now = new Date();
-    let date = document.querySelector(".location .date");
+    let date = document.querySelector(".place .todaysDate");
     date.innerText = dateBuilder(now);
 
-    let temp = document.querySelector(".current .temp");
+    let temp = document.querySelector(".forecast .temp");
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
 
-    let weather_el = document.querySelector(".current .weather");
+    let weather_el = document.querySelector(".forecast .weather");
     weather_el.innerText = weather.weather[0].main;
 
-    let hilow = document.querySelector(".hi-low");
+    let hilow = document.querySelector(".hiLow");
     hilow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`
 }
 
